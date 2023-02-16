d8543290679e41900d80d4fa13d677cd6e2ab166
Kaleb-EA <102551602+Kaleb-EA@users.noreply.github.com>
2023-02-16 14:42:28 -0500

    fixing script location

--- client/lab_3/script.js
+++ client/lab_3/script.js
@@ -20,9 +20,9 @@ const totalSlides = slidesArray.length;
 
 function updateSlidePosition() {
   slidesArray.forEach(slide => {
-    slide.classList.remove('visible');
-    slide.classList.add('hidden');    
-  })
+    slide.classList.remove('visible');  
+    slide.classList.add('hidden');  
+  });
 
   slides[slidePosition].classList.add('visible');
 }
@@ -50,7 +50,7 @@ function moveToPrevSlide() {
     if not, set the slidePosition to the current position minus one
   */
     if(slidePosition === 0){
-      slidePosition === totalSlides - 1;
+      slidePosition === totalSlides -1;
     } else { 
       slidePosition -= 1;
   }

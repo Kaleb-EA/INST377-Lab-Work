41766788cef66722f4a5a9319e586ae128310872
Kaleb-EA <102551602+Kaleb-EA@users.noreply.github.com>
2023-02-17 12:35:41 -0500

    updated prev code

--- client/lab_3/script.js
+++ client/lab_3/script.js
@@ -50,7 +50,7 @@ function moveToPrevSlide() {
     if not, set the slidePosition to the current position minus one
   */
   if(slidePosition === 0){
-    slidePosition = 3;
+    slidePosition = totalSlides - 1;
   } else { 
     slidePosition -= 1;
 } 
